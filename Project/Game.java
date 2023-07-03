f3ea0d8a3bd3e516dfe9eab29174379ae4e90fb6
Ushika <ushika.jugessur3@umail.uom.ac.mu>
2023-07-03 14:43:02 +0400

    update GUI - title screen

--- Project/Game.java
+++ Project/Game.java
@@ -22,25 +22,29 @@ public class Game{
 	JLabel titleNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
 	Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
 	Font normalFont = new Font("Times New Roman", Font.PLAIN, 28);
-	// JButton startButton, choice1, choice2, choice3, choice4;
+	JButton startButton, choice1, choice2, choice3, choice4;
 	JTextArea mainTextArea;
-    // int playerHP, monsterHP, silverRing;
+	// int playerHP, monsterHP;
 	// String weapon, position;
-
-    //TitleScreenHandler tsHandler = new TitleScreenHandler();
+	
+	//TitleScreenHandler tsHandler = new TitleScreenHandler();
 	//ChoiceHandler choiceHandler = new ChoiceHandler();
+	
+	//ImageIcon logo = new ImageIcon(".//res//jackfrost.jpg");
 
-    public static void main(String[] args){
-        new Game();
-    }
-
-    public Game(){
+	public static void main(String[] args) {
+		new Game();
+	}
+	
+	public Game(){
 		
 		window = new JFrame();
 		window.setSize(800, 600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.getContentPane().setBackground(Color.black);
 		window.setLayout(null);
+		//window.setIconImage(logo.getImage());
+		con = window.getContentPane();
 		
 		titleNamePanel = new JPanel();
 		titleNamePanel.setBounds(100, 100, 600, 150);
@@ -53,18 +57,18 @@ public class Game{
 		startButtonPanel.setBounds(300, 400, 200, 100);
 		startButtonPanel.setBackground(Color.black);
 		
-		// startButton = new JButton("START");
-		// startButton.setBackground(Color.black);
-		// startButton.setForeground(Color.white);
-		// startButton.setFont(normalFont);
-		// //startButton.addActionListener(tsHandler);
-		// startButton.setFocusPainted(false);
+		startButton = new JButton("START");
+		startButton.setBackground(Color.black);
+		startButton.setForeground(Color.white);
+		startButton.setFont(normalFont);
+		//startButton.addActionListener(tsHandler);
+		//startButton.setFocusPainted(false);
 		
-		// titleNamePanel.add(titleNameLabel);
-		// startButtonPanel.add(startButton);
+		titleNamePanel.add(titleNameLabel);
+		startButtonPanel.add(startButton);
 		
-		// con.add(titleNamePanel);
-		// con.add(startButtonPanel);
+		con.add(titleNamePanel);
+		con.add(startButtonPanel);
 		
 		window.setVisible(true);
 	}
