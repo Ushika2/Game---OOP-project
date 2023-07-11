4f286c05b486c44d2bec37a278458aeba15889e8
Ushika <ushika.jugessur3@umail.uom.ac.mu>
2023-07-11 12:50:31 +0400

    update mage - adding weapon

--- Project/character/Mage.java
+++ Project/character/Mage.java
@@ -1,12 +1,15 @@
 package character;
 import spell.*;
+import weapon.*;
 
 public class Mage extends Character{
     private Spell spell;
+    private Weapon weapon;
 
-    public Mage(String name, int HP, Spell spell){
+    public Mage(String name, int HP, Spell spell, Weapon weapon){
         super(name, HP);
         this.spell = spell;
+        this.weapon = weapon;
     }
 
     public Spell getSpell(){
@@ -17,13 +20,21 @@ public class Mage extends Character{
         this.spell = spell;
     }
 
+    public Weapon getWeapon(){
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon){
+        this.weapon = weapon;
+    }
+
     public void takeDamage(int damage){
         setHP(getHP() - damage);
         System.out.println(getName() + "takes" + damage + "damage.");
     }
 
     public void attack(Character target){
-        int totalDamage = spell.getDamage();
+        int totalDamage = spell.getDamage() + weapon.getDamage();
         target.takeDamage(totalDamage);
         System.out.println(getName() + "cast spell on" + target.getName() + "causing" + totalDamage + "damage.");
     }
